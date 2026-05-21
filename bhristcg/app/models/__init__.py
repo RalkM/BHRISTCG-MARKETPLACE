48e7cc0f7034b47c157b649a3a670e4f703df666
RalkM <kiritomunsayac@gmail.com>
2026-05-22 09:41:29 +1200

    Refactor models package to include necessary imports
    Import user, card, listing, collection, message, and review models.

--- bhristcg/app/models/__init__.py
+++ bhristcg/app/models/__init__.py
@@ -1,2 +1,6 @@
-# Models package
-# Will contain database entity models 
+from app.models.user import User
+from app.models.card import Card
+from app.models.listing import Listing, CartItem, Transaction
+from app.models.collection import Collection, CollectionItem
+from app.models.message import Message, Notification
+from app.models.review import Review, Report
